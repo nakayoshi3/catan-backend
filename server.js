f0b9eb10e1796e99520048d9ca4d25345daa3398
須田太斗 <taito@sudafutoshitonoMacBook-Air.local>
2024-12-06 03:40:01 +0900

    r

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const io = socketIo(server, {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL || "http://localhost:3000" || "https://katan-project-ef08acf343f3.herokuapp.com/"); // こちらも環境変数で管理
+  res.header("Access-Control-Allow-Origin", "https://katan-project-ef08acf343f3.herokuapp.com/"); // こちらも環境変数で管理 process.env.FRONTEND_URL || "http://localhost:3000" ||
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
